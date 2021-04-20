f6f0bbb85b689609e86941aab3d93a25bbc402a6
Pozhinskiy Vasiliy <vpozhinskii@mail.ru>
2021-04-20 15:01:02 +0300

    add search with delay

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -5,6 +5,7 @@ import {useAppDispatch, useAppSelector} from "../../utils/hooks";
 const Search = () => {
     const dispatch = useAppDispatch()
     const [searchInput, setSearchInput] = useState("")
+    const [searchInputTimer, setSearchInputTimer] = useState<number | null>(null)
     const booksPerLoad = useAppSelector(state => state.booksReducer.booksPerLoad)
 
     const searchBooks = (query: string) => {
@@ -13,9 +14,26 @@ const Search = () => {
         dispatch(getBooksThunk(query, 1, booksPerLoad))
     }
 
+    const searchWithDelay = (query: string) => {
+        dispatch(setCurrentPage(1))
+        dispatch(getBooksThunk(query, 1, booksPerLoad))
+    }
+
+    const searchBooksOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (searchInputTimer) {
+            window.clearTimeout(searchInputTimer)
+        }
+
+        const query = e.target.value
+        setSearchInput(query)
+        dispatch(setQuery(query))
+        const timerId = window.setTimeout(() => searchWithDelay(query), 1000)
+        setSearchInputTimer(timerId)
+    }
+
     return <div>
         <span>Поиск книг:</span>
-        <input onChange={e=>setSearchInput(e.target.value)} value={searchInput}/>
+        <input onChange={searchBooksOnChange} value={searchInput}/>
         <button onClick={() => searchBooks(searchInput)}>Искать</button>
     </div>
 };
